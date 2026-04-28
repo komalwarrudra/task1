6ecab324b83cece53d3559a11523b72188f8e535
komalwarrudra <komalwarrudra@gmail.com>
2026-04-28 17:23:25 +0530

    completed objective

--- task1.py
+++ task1.py
@@ -24,4 +24,30 @@ n = int(input("Enter a number to find its factorial: "))
 print(f"The factorial of {n} is {factorial(n)}")
 
 #fabonacci sequence
+def fab(num):
+    a, b = 0, 1
+    for i in range(num):
+        print(a, end=' ')
+        a, b = b, a + b
+    print()  
+num = int(input("Number of terms for Fibonacci sequence: "))
+fab(num)    
 
+#reverse string
+s = input("Enter a string to reverse: ")
+reversed_string = ""
+for char in s:
+    reversed_string = char + reversed_string
+print("Reversed string:", reversed_string)
+
+#palindrome checker
+s = input("enter to check palindrom ")
+reversed_s = ""
+for char in s:
+    reversed_s = char + reversed_s
+print(reversed_s)
+
+if s == reversed_s:
+    print("The string is a palindrome.")
+else:
+    print("not a palindrome")
